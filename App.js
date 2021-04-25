b7e93c77bf977cf89f06d533ef3070df659eedab
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-25 21:01:20 +0530

    Create the stack navigator with react navigation 4(will be updated to 5 later)

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -24,8 +24,6 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator />
   );
 }
