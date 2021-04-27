43a6e7ca790e493c4974645cec685b2f5f2b1b70
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-27 13:10:37 +0530

    Add the favorites stack navigation with the meal list component

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import {
-  createAppContainer
+    createAppContainer
 } from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
@@ -15,64 +15,75 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
+const defaultStackNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+    headerTitle: 'A Screen'
+};
+
 const MealsNavigator = createStackNavigator(
     {
-      Categories: {
-        screen: CategoriesScreen
-      },
-      CategoryMeals: {
-        screen: CategoryMealsScreen
-      },
-      MealDetail: MealDetailScreen
+        Categories: {
+            screen: CategoriesScreen
+        },
+        CategoryMeals: {
+            screen: CategoryMealsScreen
+        },
+        MealDetail: MealDetailScreen
     },
     {
-      defaultNavigationOptions: {
-        headerStyle: {
-          backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-        },
-        headerTintColor:
-          Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-        headerTitle: 'A Screen'
-      }
+        defaultNavigationOptions: defaultStackNavOptions
     }
-  );
-  
-  const tabScreenConfig = {
+);
+
+const FavNavigator = createStackNavigator(
+    {
+        Favorites: FavoritesScreen,
+        MealDetail: MealDetailScreen
+    },
+    {
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
+
+const tabScreenConfig = {
     Meals: {
-      screen: MealsNavigator,
-      navigationOptions: {
-        tabBarIcon: tabInfo => {
-          return (
-            <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
-          );
-        },
-        tabBarColor: Colors.primaryColor
-      }
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: tabInfo => {
+                return (
+                    <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+                );
+            },
+            tabBarColor: Colors.primaryColor
+        }
     },
     Favorites: {
-      screen: FavoritesScreen,
-      navigationOptions: {
-        tabBarIcon: tabInfo => {
-          return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
-        },
-        tabBarColor: Colors.accentColor
-      }
+        screen: FavNavigator,
+        navigationOptions: {
+            tabBarIcon: tabInfo => {
+                return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+            },
+            tabBarColor: Colors.accentColor
+        }
     }
-  };
-  
-  const MealsFavTabNavigator =
+};
+
+const MealsFavTabNavigator =
     Platform.OS === 'android'
-      ? createMaterialBottomTabNavigator(tabScreenConfig, {
-          activeColor: 'white',
-          shifting: true,
-          barStyle: {
-            backgroundColor: Colors.primaryColor
-          }
+        ? createMaterialBottomTabNavigator(tabScreenConfig, {
+            activeTintColor: 'white',
+            shifting: true,
+            barStyle: {
+                backgroundColor: Colors.primaryColor
+            }
         })
-      : createBottomTabNavigator(tabScreenConfig, {
-          tabBarOptions: {
-            activeTintColor: Colors.accentColor
-          }
+        : createBottomTabNavigator(tabScreenConfig, {
+            tabBarOptions: {
+                activeTintColor: Colors.accentColor
+            }
         });
-  
-  export default createAppContainer(MealsFavTabNavigator);
+
+export default createAppContainer(MealsFavTabNavigator);
